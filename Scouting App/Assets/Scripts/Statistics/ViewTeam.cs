64516503cc12053531c4e17ec3545def94282f3f
FishDog5000 <the.programmer.ninja.3.14@gmail.com>
2018-01-01 12:06:02 -0500

    Adjust view_team page

--- Scouting App/Assets/Scripts/Statistics/ViewTeam.cs
+++ Scouting App/Assets/Scripts/Statistics/ViewTeam.cs
@@ -7,7 +7,7 @@ public class ViewTeam : MonoBehaviour
 {
 	public Text TeamName;
 	public Text TeamNumber;
-	public Text AvgTemplate;
+	public TextValueItem AvgTemplate;
 
 	void Start()
 	{
@@ -21,22 +21,30 @@ public class ViewTeam : MonoBehaviour
 		if (!string.IsNullOrEmpty(team.TeamName))
 			TeamName.text = team.TeamName;
 		TeamNumber.text = "Team " + team.TeamNum.ToString();
-		Text autoMoveAvg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
-		Text autoItem1Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
-		Text autoItem2Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
-		Text item1Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
-		Text item2Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
-		Text endgameAvg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		AvgTemplate.gameObject.SetActive(true);
+		TextValueItem autoMoveAvg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		TextValueItem autoItem1Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		TextValueItem autoItem2Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		TextValueItem item1Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		TextValueItem item2Avg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		TextValueItem endgameAvg = Instantiate(AvgTemplate, AvgTemplate.transform.parent);
+		AvgTemplate.gameObject.SetActive(false);
 
-		autoMoveAvg.text = "Moves in auto " + ToPercent(team.MovedInAutoAvg) + " of the time";
-		autoItem1Avg.text = "Average balls scored in auto: " + ToRoundStr(team.AutoItem1Avg);
-		autoItem2Avg.text = "Scores a gear in auto " + ToPercent(team.AutoItem2Avg) + " of the time";
+		autoMoveAvg.KeyText.text = "% of the time moves in auto: ";
+		autoMoveAvg.ValueText.text = ToPercent(team.MovedInAutoAvg);
+		autoItem1Avg.KeyText.text = "Average balls scored in auto: ";
+		autoItem1Avg.ValueText.text = ToRoundStr(team.AutoItem1Avg);
+		autoItem2Avg.KeyText.text = "% times auto gear scored: ";
+		autoItem2Avg.ValueText.text = ToPercent(team.AutoItem2Avg);
 
-		item1Avg.text = "Average balls scored: " + ToRoundStr(team.Item1Avg);
-		item2Avg.text = "Average gears scored: " + ToRoundStr(team.Item2Avg);
-		endgameAvg.text = "Climbs the rope " + ToPercent(team.EndgameAvg) + " of the time";
+		item1Avg.KeyText.text = "Average balls scored: ";
+		item1Avg.ValueText.text = ToRoundStr(team.Item1Avg);
+		item2Avg.KeyText.text = "Average gears scored: ";
+		item2Avg.ValueText.text = ToRoundStr(team.Item2Avg);
+		endgameAvg.KeyText.text = "% times climbed rope: ";
+		endgameAvg.ValueText.text = ToPercent(team.EndgameAvg);
 
-		Destroy(AvgTemplate.gameObject);
+		//Destroy(AvgTemplate.gameObject);
 	}
 
 	private static string ToPercent(double d)
